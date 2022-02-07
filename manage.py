365e57b9902599832b06f899d8623e212d18f9cb
zapouria <33811447+zapouria@users.noreply.github.com>
2022-02-07 10:15:21 -0500

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
+    0/0
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "restAPI.settings")
     try:
         from django.core.management import execute_from_command_line
