330efc0854fbbc6c999b19fd68f4d79c06598f38
zapouria <33811447+zapouria@users.noreply.github.com>
2022-02-07 10:01:00 -0500

    Update models.py

--- polls/models.py
+++ polls/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Question(models.Model):
     question = models.CharField(max_length=100)
     pub_date = models.DateTimeField(auto_now=True)
-test
     def __str__(self):
         return self.question
 
