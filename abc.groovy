ff7bb0ea2448514bfdaee2624ca6f9177316fd6e
zapouria <33811447+zapouria@users.noreply.github.com>
2022-02-08 16:58:51 -0500

    Update abc.groovy

--- abc.groovy
+++ abc.groovy
@@ -1,3 +1,7 @@
 def load(){
-  println("Hello")
+  def postmanGet = new URL("http://10.0.0.54:9000/api/issues/search?types=BUG&componentKeys=jenkin-test&resolved=false")
+  def getConnection = postmanGet.openConnection()
+  getConnection.requestMethod = 'GET'
+  assert getConnection.responseCode == 200
+  return 1
 }
