3f9ee6fad65e7f2d72ef7083f879830e1273b38b
zapouria <33811447+zapouria@users.noreply.github.com>
2022-02-08 13:09:04 -0500

    Update abc.groovy

--- abc.groovy
+++ abc.groovy
@@ -1,1 +1,4 @@
-echo "test"
+def postmanGet = new URL("http://0.0.0.0:9000/api/issues/search?types=BUG&componentKeys=jenkin-test&resolved=false")
+def getConnection = postmanGet.openConnection()
+getConnection.requestMethod = 'GET'
+assert getConnection.responseCode == 200
