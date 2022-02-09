17c77c790a54fbef6bdf115b92e57aa63f84b57c
zapouria <33811447+zapouria@users.noreply.github.com>
2022-02-09 09:31:50 -0500

    Update abc.groovy

--- abc.groovy
+++ abc.groovy
@@ -4,8 +4,8 @@ def runApp(){
   getConnection.requestMethod = 'GET'
   InputStream inputStream = postmanGet.getInputStream();
   
-  String originalString = randomAlphabetic(DEFAULT_SIZE);
-  InputStream inputStream = new ByteArrayInputStream(originalString.getBytes());
+//   String originalString = randomAlphabetic(DEFAULT_SIZE);
+//   InputStream inputStream = new ByteArrayInputStream(originalString.getBytes());
 
   String text = new BufferedReader(
       new InputStreamReader(inputStream, StandardCharsets.UTF_8))
