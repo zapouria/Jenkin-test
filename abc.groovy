af6f86ab77eb89882a5e8aa233ee40a78340d8cc
zapouria <33811447+zapouria@users.noreply.github.com>
2022-02-09 09:30:15 -0500

    Update abc.groovy

--- abc.groovy
+++ abc.groovy
@@ -2,11 +2,16 @@ def runApp(){
   def postmanGet = new URL("http://10.0.0.54:9000/api/issues/search?types=BUG&componentKeys=jenkin-test&resolved=false")
   def getConnection = postmanGet.openConnection()
   getConnection.requestMethod = 'GET'
-  br = new BufferedReader(new InputStreamReader(postmanGet.getInputStream()))
-  String strCurrentLine
-  while ((strCurrentLine = br.readLine()) != null) {
-    println(strCurrentLine);
-  }
+  InputStream inputStream = postmanGet.getInputStream();
+  
+  String originalString = randomAlphabetic(DEFAULT_SIZE);
+  InputStream inputStream = new ByteArrayInputStream(originalString.getBytes());
+
+  String text = new BufferedReader(
+      new InputStreamReader(inputStream, StandardCharsets.UTF_8))
+      .lines()
+      .collect(Collectors.joining("\n"));
+      
   assert getConnection.responseCode == 200
 }
 
